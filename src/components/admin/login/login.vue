3dfcd5c0ffe88f329a658578714fc77551ca2d3e
Loek Appel <loek@appel-snijder.nl>
2023-05-20 10:25:18 +0200

    begin html inlog

--- src/components/admin/login/login.vue
+++ src/components/admin/login/login.vue
@@ -1,17 +1,24 @@
 <template>
-  <div>
-      <LoginForm></LoginForm>
-  </div>
+    <form>
+        <div class="form-control">
+            <label for="email">E-mail</label>
+            <input type="email" id="email">
+        </div>
+        <div class="form-control">
+            <label for="password">password</label>
+            <input type="password" id="password">
+        </div>
+        <base-button> Login </base-button>
+        <base-Button type="button" mode="flat"> register</base-Button>
+    </form>
 </template>
 
 <script>
 import axios from "axios";
-import LoginForm from "@/components/admin/login/login-form/login-form.vue";
-import {AuthenticationRequest} from "@/components/core/models/AuthenticationRequest";
+import { AuthenticationRequest } from "@/components/core/models/AuthenticationRequest";
 
 export default {
     name: "login-component",
-    components: {LoginForm},
     data() {
         return {
             loggedInCheck: Boolean,
