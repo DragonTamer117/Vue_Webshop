9d63a3468ce1809fa294a93cd3352cd350c9a387
Loek Appel <loek@appel-snijder.nl>
2023-06-02 23:06:01 +0200

    basic sendOrder

--- src/components/shared/order/shopping-cart.vue
+++ src/components/shared/order/shopping-cart.vue
@@ -4,6 +4,15 @@
       <div>
         totale kosten: {{ totalPrice }}
       </div>
+      <div>
+        <button
+            class="inline-block align-middle text-center select-none border font-normal whitespace-no-wrap
+                  rounded py-1 px-3 leading-normal no-underline bg-blue-600 text-white hover:bg-blue-600 onAddToCart"
+            @click=sendOrder(this.storageProduct)
+        >
+          In Winkelwagen
+        </button>
+      </div>
       <div class="sm:w-full pr-4 pl-4">
         <cart-items
             v-for="item in storageProduct"
@@ -20,6 +29,7 @@
 import ShopItem from "@/components/shop/shop-item/shop-item.vue";
 import {StoreVars} from "@/components/shop/shop-item/StoreVars";
 import CartItems from "@/components/shared/order/cart-items/cart-items.vue";
+import axios from "axios";
 
 export default {
   name: "shopping-cart-component",
@@ -44,6 +54,12 @@ export default {
         let product = this.storageProduct[i];
         this.totalPrice = this.totalPrice + product.price;
       }
+    },
+    sendOrder(){
+      const config = {
+        headers: { Authorization: `Bearer ${this.token}`}
+      };
+      const response = await axios.get('http://localhost:8080/api/v1/users/' + id, config);
     }
   },
   beforeMount() {
