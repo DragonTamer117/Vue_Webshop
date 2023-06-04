792da0765b44c8b0242142092ed885663c53075f
Loek Appel <loek@appel-snijder.nl>
2023-06-04 21:11:25 +0200

    added bling bling to cart items

--- src/components/shared/order/cart-items/cart-items.vue
+++ src/components/shared/order/cart-items/cart-items.vue
@@ -13,17 +13,30 @@
         </span>
         </div>
         <div class="sm:w-1/2 pr-4 pl-4">
-          <div class="flex flex-wrap ">
-            <h4>{{ storageProduct.name }}</h4> <br>
-            <h4>&euro;&nbsp;{{ storageProduct.price }}</h4>
+          <div class="flex flex-col ">
+            <p>{{ storageProduct.name }}</p>
+            <p>&euro; {{ storageProduct.price }}</p>
+            <button @click="removeFromOrder(storageProduct)">
+              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                   class="bi bi-trash border hover:text-red-500 hover:border-red-500 h-6 w-6"
+                   viewBox="0 0 16 16">
+                <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5
+                0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
+                <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0
+                1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0
+                1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>
+              </svg>
+            </button>
           </div>
         </div>
       </div>
     </div>
   </a>
 </template>
+
 <script>
-import {defineComponent} from 'vue'
+import { defineComponent } from 'vue';
+import { StoreVars } from "@/components/shop/shop-item/StoreVars";
 
 export default defineComponent({
   name: "cart-items",
