b04776cf17a0cbc497489be2d2acf0fffd4887ff
Loek Appel <loek@appel-snijder.nl>
2023-06-04 21:10:39 +0200

    clean up of shop cart and working post request

--- src/components/shared/order/shopping-cart.vue
+++ src/components/shared/order/shopping-cart.vue
@@ -1,44 +1,52 @@
 <template>
-  <div>
-    <div class="flex flex-wrap ">
-      <div>
-        totale kosten: {{ totalPrice }}
+  <div v-if="this.token.length >= 1" class="flex flex-col items-center">
+    <div class="sm:w-full pr-4 pl-4">
+      <cart-items
+          v-for="item in storageProduct"
+          v-bind:key="item.id"
+          :storage-product="item"
+      ></cart-items>
+    </div>
+    <div class="flex flex-wrap items-center">
+      <div class="text-center mx-auto mb-4 font-bold mt-4 mr-4">
+        Totale kosten: &euro; {{ totalPrice }},-
       </div>
-      <div>
+      <div class="text-center mx-auto">
         <button
             class="inline-block align-middle text-center select-none border font-normal whitespace-no-wrap
                   rounded py-1 px-3 leading-normal no-underline bg-blue-600 text-white hover:bg-blue-600 onAddToCart"
-            @click=sendOrder(this.storageProduct)
+            @click="sendOrder(storageProduct)"
         >
-          In Winkelwagen
+          Bestellen
         </button>
-      </div>
-      <div class="sm:w-full pr-4 pl-4">
-        <cart-items
-            v-for="item in storageProduct"
-            v-bind:key="item.id"
-            :storage-product="item"
-        ></cart-items>
       </div>
     </div>
   </div>
+  <div v-if="this.token.length <= 0" class="flex flex-col items-center justify-center h-screen">
+    <router-link
+        :to="{ path: `login` }"
+        class="inline-block align-middle text-center select-none border font-normal whitespace-no-wrap
+                  rounded py-1 px-3 leading-normal no-underline bg-blue-600 text-white hover:bg-blue-600"
+    >
+      U moet inloggen of de bestelling af te ronden.
+    </router-link>
+  </div>
 </template>
 
-<script>
 
-import ShopItem from "@/components/shop/shop-item/shop-item.vue";
-import {StoreVars} from "@/components/shop/shop-item/StoreVars";
+<script>
+import { StoreVars } from "@/components/shop/shop-item/StoreVars";
 import CartItems from "@/components/shared/order/cart-items/cart-items.vue";
 import axios from "axios";
 
 export default {
   name: "shopping-cart-component",
-  // eslint-disable-next-line vue/no-unused-components
-  components: { CartItems, ShopItem },
+  components: { CartItems },
   data() {
     return {
       storageProduct: [],
-      totalPrice: 0
+      totalPrice: 0,
+      token: ''
     }
   },
   methods: {
@@ -46,10 +54,9 @@ export default {
       // let order: Order;
       const store = StoreVars();
       this.storageProduct = store.storageProduct;
-      console.log(this.storageProduct);
       this.calcTotalPrice();
     },
-    calcTotalPrice(){
+    calcTotalPrice() {
       for (let i = 0; i < this.storageProduct.length; i++) {
         let product = this.storageProduct[i];
         this.totalPrice = this.totalPrice + product.price;
@@ -59,11 +66,22 @@ export default {
       const config = {
         headers: { Authorization: `Bearer ${this.token}`}
       };
-      const response = await axios.get('http://localhost:8080/api/v1/users/' + id, config);
+      const requestBody = {
+        products: this.storageProduct.map(product => product.id)
+      }
+
+      console.log(config);
+
+      axios.post('http://localhost:8080/api/v1/orders', requestBody, config);
+    },
+    getToken() {
+      const store = StoreVars();
+      this.token = store.token;
     }
   },
   beforeMount() {
     this.getProductsFromOrder();
+    this.getToken();
   }
 }
 </script>
