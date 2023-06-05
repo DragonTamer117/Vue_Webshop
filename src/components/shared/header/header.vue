d77a96c64824a08da318d1d4dec16b54f6ad4726
Loek Appel <loek@appel-snijder.nl>
2023-06-05 14:01:44 +0200

    added account and admin routes

--- src/components/shared/header/header.vue
+++ src/components/shared/header/header.vue
@@ -41,6 +41,20 @@
               </span>
             </button>
           </router-link>
+          <router-link :to="{ path: `account` }"
+                       class="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0
+          md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white
+          md:dark:hover:bg-transparent active:text-bg-gray-900"
+          >
+            account
+          </router-link>
+          <router-link :to="{ path: `admin` }"
+                       class="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0
+          md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white
+          md:dark:hover:bg-transparent active:text-bg-gray-900"
+          >
+            beheer
+          </router-link>
         </ul>
       </div>
     </div>
