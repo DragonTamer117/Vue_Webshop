96cbba8a483e0de9a12ad778a783f5c3ca964b0d
Loek Appel <loek@appel-snijder.nl>
2023-06-05 14:01:18 +0200

    fixed error customer

---
+++ src/components/account/customer/customer.vue
@@ -0,0 +1,17 @@
+<template>
+  <P>je moeder</P>
+</template>
+
+<script>
+import {defineComponent} from 'vue'
+
+export default defineComponent({
+  name: "customer-component"
+})
+</script>
+
+
+
+<style scoped>
+
+</style>
