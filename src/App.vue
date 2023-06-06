26983708c623650294365c1350904066c1f14e71
Loek Appel <loek@appel-snijder.nl>
2023-06-06 16:45:50 +0200

    head is now fixed to the top

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,12 @@
 <template>
   <div class="flex min-h-screen flex-col h-auto m-0">
-    <app-navbar></app-navbar>
-    <div>
+    <app-navbar class="fixed top-0 left-0 right-0 z-10"></app-navbar>
+    <div class="mt-16">
       <router-view></router-view>
     </div>
   </div>
 </template>
+
 
 <script>
 import AppNavbar from "@/components/shared/header/header.vue";
