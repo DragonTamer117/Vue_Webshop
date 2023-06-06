38a191e043b17ceb2618a9a2dde92a6c20f19d14
Loek Appel <loek@appel-snijder.nl>
2023-06-06 16:47:11 +0200

    buttons looking now better

--- src/components/shared/header/header.vue
+++ src/components/shared/header/header.vue
@@ -1,9 +1,9 @@
 <template>
   <nav class="bg-white border-gray-200 px-2 sm:px-4 dark:bg-sky-900">
     <div class="container flex flex-wrap items-center justify-between">
-      <a class="flex items-center">
+      <router-link :to="{ path: '/' }" class="flex items-center">
         <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white italic pl-20">EbikeNL</span>
-      </a>
+      </router-link>
       <div class="group-aria-hidden:w-full md:block md:w-auto">
         <ul
             class="flex flex-col p-4 mt-4 border border-gray-100 rounded-lg dark:bg-sky-900 md:flex-row md:space-x-8 md:mt-0
@@ -17,11 +17,28 @@
             Shop
           </router-link>
           <router-link :to="{ path: `login` }"
+                       @click="logout()"
               class="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0
+          md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white
+          md:dark:hover:bg-transparent active:text-bg-gray-900 font-bold"
+          >
+            {{ isAuthenticated ? 'Logout' : 'Login' }}
+          </router-link>
+          <router-link :to="{ path: `account` }"
+                       v-if="isAuthenticated === true"
+                       class="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0
           md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white
           md:dark:hover:bg-transparent active:text-bg-gray-900"
           >
-            Login
+            Account
+          </router-link>
+          <router-link :to="{ path: `admin` }"
+                       v-if="isAuthenticated === true && (role === 'ADMIN' || role === 'MODERATOR')"
+                       class="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0
+          md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white
+          md:dark:hover:bg-transparent active:text-bg-gray-900"
+          >
+            Beheer
           </router-link>
           <router-link :to="{ path: 'order' }" class="font-sans block mt-4 lg:inline-block lg:mt-0 lg:ml-6 align-middle text-black hover:text-gray-700">
             <button class="relative flex">
@@ -35,25 +52,11 @@
               </svg>
               <span
                   class="absolute right-0 top-0 rounded-full bg-red-600 w-4 h-4 top right p-0 m-0 text-white font-mono
-              text-sm  leading-tight text-center" v-if="storageProduct.length !== 0"
+              text-sm  leading-tight text-center" v-if="products.length !== 0"
               >
-                {{ storageProduct.length }}
+                {{ products.length }}
               </span>
             </button>
-          </router-link>
-          <router-link :to="{ path: `account` }"
-                       class="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0
-          md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white
-          md:dark:hover:bg-transparent active:text-bg-gray-900"
-          >
-            account
-          </router-link>
-          <router-link :to="{ path: `admin` }"
-                       class="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0
-          md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white
-          md:dark:hover:bg-transparent active:text-bg-gray-900"
-          >
-            beheer
           </router-link>
         </ul>
       </div>
@@ -62,25 +65,32 @@
 </template>
 
 <script>
+import { auth } from "@/services/auth";
 import { StoreVars } from "@/components/shop/shop-item/StoreVars";
 
 export default {
   name: 'app-navbar',
-  data() {
-    return {
-      storageProduct: []
+  computed: {
+    products() {
+      return StoreVars().storageProduct;
+    },
+    isAuthenticated() {
+      return auth.state.isAuthenticated;
+    },
+    role() {
+      return auth.state.role;
     }
   },
   methods: {
-    getProductsFromOrder() {
-      this.storageProduct = StoreVars().storageProduct;
+    logout() {
+      if (this.isAuthenticated === true) {
+        auth.logout();
+      }
     }
-  },
-  beforeMount() {
-    this.getProductsFromOrder();
   }
 };
 </script>
+
 
 <style>
 </style>
