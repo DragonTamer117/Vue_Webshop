c6ff318ae0d9884f814ef38bb0835d4eec3ae82a
Loek Appel <loek@appel-snijder.nl>
2023-06-06 16:46:34 +0200

    customer view added

--- src/components/account/customer/customer.vue
+++ src/components/account/customer/customer.vue
@@ -1,17 +1,26 @@
 <template>
-  <P>je moeder</P>
+  <account-details></account-details>
+  <order-details></order-details>
 </template>
 
 <script>
-import {defineComponent} from 'vue'
+import OrderDetails from "@/components/account/customer/order-details/order-details.vue";
+import AccountDetails from "@/components/account/customer/account-details/account-details.vue";
 
-export default defineComponent({
-  name: "customer-component"
-})
+export default {
+  name: "customer-component",
+  components: { AccountDetails, OrderDetails },
+  data() {
+    return {
+
+    };
+  },
+  methods: {
+
+  }
+};
 </script>
 
-
-
 <style scoped>
-
+/* Add your custom styles here */
 </style>
