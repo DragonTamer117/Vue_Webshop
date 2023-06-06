3a959b40abee6e61803b4a9e21537f2c8ce6c2af
Loek Appel <loek@appel-snijder.nl>
2023-06-06 14:07:42 +0200

    send order refactored

--- src/components/shared/order/shopping-cart.vue
+++ src/components/shared/order/shopping-cart.vue
@@ -1,28 +1,30 @@
 <template>
-  <div v-if="this.token.length >= 1" class="flex flex-col items-center">
-    <div class="sm:w-full pr-4 pl-4">
+  <div v-if="token.length >= 1" class="flex flex-col items-center relative">
+    <div class="sm:w-full">
       <cart-items
           v-for="item in storageProduct"
           v-bind:key="item.id"
           :storage-product="item"
       ></cart-items>
     </div>
-    <div class="flex flex-wrap items-center">
-      <div class="text-center mx-auto mb-4 font-bold mt-4 mr-4">
+    <div class="right-0 bottom-3 mr-4 mt-4 fixed">
+      <div class="text-center font-bold">
         Totale kosten: &euro; {{ totalPrice }},-
       </div>
-      <div class="text-center mx-auto">
-        <button
-            class="inline-block align-middle text-center select-none border font-normal whitespace-no-wrap
-                  rounded py-1 px-3 leading-normal no-underline bg-blue-600 text-white hover:bg-blue-600 onAddToCart"
-            @click="sendOrder(storageProduct)"
-        >
-          Bestellen
-        </button>
-      </div>
+      <button
+          :class="[
+              'inline-block align-middle text-center select-none border font-normal whitespace-no-wrap rounded py-1 ' +
+              'px-3 leading-normal no-underline',
+              storageProduct.length > 0 ? 'bg-blue-600 text-white hover:bg-blue-600' : 'bg-gray-400 text-gray-600 cursor-not-allowed'
+              ]"
+          @click="sendOrder(storageProduct)"
+          :disabled="storageProduct.length < 1"
+      >
+        Bestellen
+      </button>
     </div>
   </div>
-  <div v-if="this.token.length <= 0" class="flex flex-col items-center justify-center h-screen">
+  <div v-if="token.length <= 0" class="flex flex-col items-center justify-center h-screen">
     <router-link
         :to="{ path: `login` }"
         class="inline-block align-middle text-center select-none border font-normal whitespace-no-wrap
@@ -46,7 +48,7 @@ export default {
     return {
       storageProduct: [],
       totalPrice: 0,
-      token: ''
+      token: ""
     }
   },
   methods: {
@@ -61,7 +63,7 @@ export default {
         this.totalPrice += product.price;
       }
     },
-    sendOrder() {
+    async sendOrder() {
       const config = {
         headers: { Authorization: `Bearer ${this.token}`}
       };
