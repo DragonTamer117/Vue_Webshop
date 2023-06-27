72213a15ee839dba7bd4ee43113ce993dbe86c8c
Ali Basiri <57348384+DragonTamer117@users.noreply.github.com>
2023-06-27 11:02:28 +0200

    Changed the map order, so imports changed.

--- src/components/account/customer/customer.vue
+++ src/components/account/customer/customer.vue
@@ -4,8 +4,8 @@
 </template>
 
 <script>
-import OrderDetails from "@/components/account/customer/order-details/order-details.vue";
-import AccountDetails from "@/components/account/customer/account-details/account-details.vue";
+import OrderDetails from "@/components/account/account-shared/order-details/order-details.vue";
+import AccountDetails from "@/components/account/account-shared/account-details/account-details.vue";
 
 export default {
   name: "customer-component",
