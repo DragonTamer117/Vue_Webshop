e030bb3d2ec6343e5092da2d1cacb867697330c1
Ali Basiri <57348384+DragonTamer117@users.noreply.github.com>
2023-06-27 10:53:01 +0200

    Implemented Dark mode for the whole webshop. So that we can program in the evening/night. Plus made some slight changes to the order of the header.

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,11 @@
 <template>
   <div class="flex min-h-screen flex-col h-auto m-0">
     <app-navbar class="fixed top-0 left-0 right-0 z-10"></app-navbar>
-    <div class="mt-16">
+    <div class="mt-80 md:mt-18">
       <router-view></router-view>
     </div>
   </div>
 </template>
-
 
 <script>
 import AppNavbar from "@/components/shared/header/header.vue";
@@ -20,4 +19,11 @@ export default {
 </script>
 
 <style>
+html.dark {
+  color-scheme: dark;
+}
+
+body {
+  @apply bg-slate-50 text-slate-800 dark:bg-slate-800 dark:text-slate-50
+}
 </style>
---
+++ src/components/shared/header/darkMode.vue
@@ -0,0 +1,28 @@
+<template>
+  <button
+      @click="toggleDark()"
+      class="flex items-center justify-center rounded px-4 py-2 cursor-pointer border
+      dark:text-yellow-700 dark:bg-yellow-400 dark:border-yellow-200 text-white bg-black border-black"
+  >
+    {{ isDark ? 'Light' : 'Dark' }}
+  </button>
+</template>
+
+<script setup>
+import { useDark, useToggle } from '@vueuse/core';
+
+const isDark = useDark({
+  onChanged(isDark) {
+    if (isDark) {
+      document.documentElement.classList.add('dark');
+    } else {
+      document.documentElement.classList.remove('dark');
+    }
+  }
+});
+const toggleDark = useToggle(isDark);
+</script>
+
+<style scoped>
+/* Add your custom styles here */
+</style>
