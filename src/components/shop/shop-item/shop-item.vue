51808b53b2713c443bdde2a1a8fea46fdd66a313
Ali Basiri <57348384+DragonTamer117@users.noreply.github.com>
2023-06-27 10:57:44 +0200

    Added a toastr, plus changed some css for Darkmode and to look better.

--- src/components/shop/shop-item/shop-item.vue
+++ src/components/shop/shop-item/shop-item.vue
@@ -1,22 +1,21 @@
 <template>
-  <a
-      class="relative block py-3 px-6 -mb-px border border-r-0 border-l-0 border-gray-300 no-underline clearfix">
+  <a class="relative block py-3 px-6 b-px border border-r-0 border-l-0 border-gray-300 no-underline clearfix">
     <div class="list-group-item-heading">
       <div class="flex flex-wrap ">
         <div class="sm:w-1/2 pr-4 pl-4">
-        <span class="pull-left">
-          <img
-              :src="storageProduct.imageUrl"
-              alt="{{ storageProduct.name }}"
-              class="img-responsive"
-              style="max-height: 200px;">
-        </span>
+          <h1 class="font-bold text-2xl">{{ storageProduct.name }}</h1>
+          <span class="pull-left">
+            <img
+                :src="storageProduct.imageUrl"
+                alt="{{ storageProduct.name }}"
+                class="img-responsive border"
+                style="max-height: 200px;"
+            />
+          </span>
         </div>
         <div class="sm:w-1/2 pr-4 pl-4">
           <div class="flex flex-wrap ">
-            <h4>{{ storageProduct.name }}</h4> <br>
-            <h4>&euro;&nbsp;{{ storageProduct.price }}</h4>
-
+            <p class="text-lg font-semibold text-blue-400">&euro; {{ storageProduct.price }},-</p>
           </div>
           <div class="flex flex-wrap ">
             <span class="pull-right">
@@ -25,7 +24,7 @@
                   rounded py-1 px-3 leading-normal no-underline bg-blue-600 text-white hover:bg-blue-600 onAddToCart"
                   @click=addProductToOrder(this.storageProduct)
               >
-                In Winkelwagen
+                Add to cart
               </button>
             </span>
           </div>
@@ -37,6 +36,8 @@
 
 <script>
 import { StoreVars } from "@/components/shop/shop-item/StoreVars";
+import { toast } from 'vue3-toastify';
+import 'vue3-toastify/dist/index.css';
 
 export default {
   name: "shop-item",
@@ -46,6 +47,13 @@ export default {
     addProductToOrder() {
       const store = StoreVars();
       store.storageProduct.push(this.storageProduct);
+      toast.success("Toegevoegd tot de winkelwagen!", {
+        autoClose: 2000,
+        position: "bottom-right",
+        closeOnClick: true,
+        closeButton: true,
+        delay: 250
+      });
     }
   }
 }
