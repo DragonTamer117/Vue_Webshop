c165c65c7611014508737d0b4b53e5d1dbfd1240
Ali Basiri <57348384+DragonTamer117@users.noreply.github.com>
2023-06-27 10:53:52 +0200

    Darkmode works with tailwind.config.js now.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
-  darkMode: false, // or 'media' or 'class'
+  darkMode: 'class', // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      spacing: {
+        '18': '4.5rem'
+      }
+    },
   },
   variants: {
     extend: {},
